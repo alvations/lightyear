c06ba14a3809f2bde8e74a0d4329e9e57deed35b
alvations <alvations@gmail.com>
2022-01-09 18:16:51 -0500

    update setup and doc

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # lightyear
+
+To infinity and beyond...
+
+Usage
+====
+
+[in]:
+
+```
+from lightyear import Buzz
+
+hyp = 'The dog bit the man.'
+ref = 'The dog had bit the man.'
+
+buzz = Buzz()
+buzz.score(hyp, ref)
+```
+
+[out]:
+
+```
+{'bert_score': 98.19023013114929,
+ 'bleu_score': 51.15078115793242,
+ 'comet_score': 100}
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  name = 'lightyear',
+  packages = ['lightyear'],
+  version = '0.0.1',
+  description = 'lightyear',
+  long_description = '',
+  author = '',
+  url = 'https://github.com/alvations/lightyear',
+  keywords = [],
+  install_requires = [bert_score, sacrebleu, unbabel-comet, pytorch_lightning],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ]
+)
