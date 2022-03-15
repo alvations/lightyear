215376744d0eb66a4dd7a442d23083739d5ecce7
alvations <alvations@gmail.com>
2022-03-15 10:04:58 -0400

    prune and quantize comet model

--- lightyear/metrics/comet.py
+++ lightyear/metrics/comet.py
@@ -1,17 +1,34 @@
 from functools import partial
 
 import torch
+from torch import nn
 from pytorch_lightning.trainer.trainer import Trainer
+from pytorch_lightning.callbacks import ModelPruning
 from torch.utils.data import DataLoader
 
 import comet
 
+def compute_amount(epoch):
+    """Apply the lottery ticket hypothesis"""
+    if epoch == 10:
+        return 0.5
+    elif epoch == 50:
+        return 0.25
+    elif 75 < epoch < 99:
+        return 0.01
+
+
 class COMETScore:
     def __init__(self, modelname='wmt20-comet-da'):
         _cuda = 1 if torch.cuda.is_available() else 0
-        self.model = comet.models.load_from_checkpoint(
-            comet.download_utils.download_model(modelname))
-        self.trainer = Trainer(gpus=_cuda, deterministic=True, logger=False)
+        self.model = torch.quantization.quantize_dynamic(
+            comet.models.load_from_checkpoint(
+                comet.download_utils.download_model(modelname)),
+             {nn.LayerNorm, nn.Linear, nn.Dropout},
+             dtype=torch.qint8)
+
+        self.trainer = Trainer(gpus=_cuda, deterministic=True, logger=False,
+            callbacks=[ModelPruning("l1_unstructured", amount=compute_amount)])
         self.prep_sample = partial(self.model.prepare_sample, inference=True)
 
     def score(self, hyp: str, ref: str, src: str=None, normalize=True, clip=True, *args, **kwargs):
@@ -20,7 +37,7 @@ class COMETScore:
             dataset=[data],
             batch_size=1,
             collate_fn=self.prep_sample,
-            num_workers=1,
+            num_workers=0,
         )
         predictions = self.trainer.predict(
             self.model, dataloaders=dataloader, return_predictions=True,
